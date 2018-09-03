9db0f7ff5eff4ae2920759be5d30cb4660a133e0
niknows <33270711+niknows@users.noreply.github.com>
2018-09-02 22:44:56 -0300

    Create script.js

---
+++ hide/script.js
@@ -0,0 +1,13 @@
+var close = document.querySelector("#close");
+var menu = document.querySelector("menu");
+var hamburger = document.querySelector("#menu");
+
+function toggle() {
+  menu.classList.toggle("invisible");
+  hamburger.classList.toggle("invisible");
+}
+
+hamburger.addEventListener("click", function() {
+  menu.classList.toggle("invisible");
+  hamburger.classList.toggle("invisible");
+});

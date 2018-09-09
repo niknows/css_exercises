cc5f313db9578af085e6b668a2e3079c0f831fa4
niknows <33270711+niknows@users.noreply.github.com>
2018-09-08 23:09:15 -0300

    Update script.js

--- grid_5/script.js
+++ grid_5/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//COLOR TOGGLE
 const container = document.querySelector(".container");
 container.addEventListener("click", function() {
   Array.from(container.children).forEach(function(item) {
